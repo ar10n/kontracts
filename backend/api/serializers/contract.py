c2191759a450d90c8e97bf35a1aed24f7e460254
Sergey Nikitin <sergey.ar10n@gmail.com>
2022-04-10 19:49:11 +0300

    modify claim serializer

--- backend/api/serializers/contract.py
+++ backend/api/serializers/contract.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 
 from ..models.contract import Contract
+from ..serializers.claim import ClaimSerializer
 from ..serializers.client import ClientSerializer
 from ..serializers.company import CompanySerializer
-from ..serializers.region import RegionSerializer
 from ..serializers.product import ProductSerializer
+from ..serializers.region import RegionSerializer
 
 
 class ContractSerializer(serializers.ModelSerializer):
@@ -12,6 +13,8 @@ class ContractSerializer(serializers.ModelSerializer):
     company = CompanySerializer()
     region = RegionSerializer()
     products = ProductSerializer(many=True)
+
+    claims = ClaimSerializer(many=True)
 
     class Meta:
         model = Contract
